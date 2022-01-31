9f11fc71759a85f170d9de7165da4f639206b745
ASITHA INDRAJITH DIVISEKARA <46625806+Divisekara@users.noreply.github.com>
2022-02-01 03:43:29 +0530

    Outputs save into a seperate folder

--- scipt.py
+++ scipt.py
@@ -1,12 +1,8 @@
 import os
 import subprocess
 
-def printing(file_list):
-    for i in file_list:
-        print(i)
-
 file_list = os.listdir(".")
-
+subprocess.run('mkdir output_videos', shell=True)
 
 video_file_names = []
 
@@ -14,19 +10,13 @@ for i in file_list:
     if(".mp4" in i):
         video_file_names.append(i.replace(".mp4",""))
 
-printing(video_file_names)
-
 for j in video_file_names:
     input_video = j + '.mp4'
     input_sub = j + ".srt"
-    output_video = j + "_srt_" + ".mp4"
+    output_video = ".\\output_videos\\" + j + "_srt_" + ".mp4"
     
     command = 'ffmpeg -i "{}" -vf subtitles="{}" "{}" '.format(input_video, input_sub, output_video)
     try:
         subprocess.run(command, shell=True)
     except:
         continue
-
-print(command)
-
-# ffmpeg -i "D:\\sample folder\\input video.mp4" -vf subtitles="D:\\sample folder\\input video.srt" "D:\\sample folder\\output video.mp4" 
